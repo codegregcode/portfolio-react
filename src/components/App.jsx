633a55296a37fe3704a02fdd46eea0ce88b7df40
Greg Cain <codegregcode@gmail.com>
2023-05-25 21:31:13 +0100

    sets up project and imports modules

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/app.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
