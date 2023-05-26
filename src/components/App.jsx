18f82310c989750b6fd251354e3d56f6a15b0311
Greg Cain <codegregcode@gmail.com>
2023-05-26 09:29:08 +0100

    updates App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Land from './Land';
+import MainMenu from './MainMenu';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio';
+import Footer from './Footer';
+
 import '../styles/app.css';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Routes>
+      <Route path="/" element={<Land />} />
+      <Route path="/menu" element={<MainMenu />} />
+      <Route path="/about-me" element={<AboutMe />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+    </Routes>
+    <Footer />
   </div>
 );
 
