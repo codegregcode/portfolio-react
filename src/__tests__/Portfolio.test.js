62863d606f9bcd1559020376c6354625fbd7dd12
Greg Cain <codegregcode@gmail.com>
2023-05-26 13:50:06 +0100

    updates and styles Portfolio and creates test

---
+++ src/__tests__/Portfolio.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import Portfolio from '../components/Portfolio';
+
+jest.mock('../firebase', () => ({
+  collection: () => ({
+    get: jest.fn(() =>
+      Promise.resolve({
+        forEach: (callback) => {
+          const mockData = [
+            {
+              id: 'project1',
+              data: () => ({
+                title: 'Project 1',
+                description: 'Description 1',
+                image_link: 'image1.jpg',
+                github_link: 'github.com/project1',
+                deploy_link: 'example.com/project1',
+              }),
+            },
+            {
+              id: 'project2',
+              data: () => ({
+                title: 'Project 2',
+                description: 'Description 2',
+                image_link: 'image2.jpg',
+                github_link: 'github.com/project2',
+                deploy_link: 'example.com/project2',
+              }),
+            },
+          ];
+          mockData.forEach(callback);
+        },
+      })
+    ),
+  }),
+}));
+
+describe('Portfolio', () => {
+  it('renders project information', async () => {
+    const { getByText, getByAltText } = render(<Portfolio />);
+
+    await waitFor(() => {
+      const project1 = getByText('Project 1');
+      const project2 = getByText('Project 2');
+      const description1 = getByText('Description 1');
+      const description2 = getByText('Description 2');
+      const image1 = getByAltText('Project 1');
+      const image2 = getByAltText('Project 2');
+
+      expect(project1).toBeInTheDocument();
+      expect(project2).toBeInTheDocument();
+      expect(description1).toBeInTheDocument();
+      expect(description2).toBeInTheDocument();
+      expect(image1).toBeInTheDocument();
+      expect(image2).toBeInTheDocument();
+    });
+  });
+});
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import db from '../firebase';
+
+import '../styles/portfolio.css';
+
+const getProjects = async () => {
+  const querySnapshot = await db.collection('projects').get();
+
+  const projects = [];
+  querySnapshot.forEach((doc) => {
+    const project = doc.data();
+    projects.push(project);
+  });
+
+  return projects;
+};
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const projectsData = await getProjects();
+      setProjects(projectsData);
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      {projects.map((project) => (
+        <div key={project.title} className="project-container">
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+          <img src={project.image_link} alt={project.title} />
+          <a href={project.github_link} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <a href={project.deploy_link} target="_blank" rel="noreferrer">
+            Link 2 Deploy
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
