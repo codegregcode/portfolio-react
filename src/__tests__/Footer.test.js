85afdd5b2345b6206725f0092bf257900b9dd385
Greg Cain <codegregcode@gmail.com>
2023-05-26 11:42:55 +0100

    updates failing tests

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 describe('Footer', () => {
-  it('renders all items in footer', () => {
-    render(<Footer />);
+  it('renders github,linkedin n email on path /', () => {
+    render(<Footer currentLocation="/" />);
 
-    const conditional = screen.getByText('Conditional Link');
     const github = screen.getByText('GitHub');
     const linkedin = screen.getByText('LinkedIn');
     const email = screen.getByText('E-Mail Me');
 
-    expect(conditional).toBeInTheDocument();
+    expect(github).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
+
+  it('renders github, linkedin n email on path /menu', () => {
+    render(<Footer currentLocation="/menu" />);
+
+    const github = screen.getByText('GitHub');
+    const linkedin = screen.getByText('LinkedIn');
+    const email = screen.getByText('E-Mail Me');
+
+    expect(github).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
+
+  it('renders about me link when on path /portfolio', () => {
+    render(
+      <MemoryRouter>
+        <Footer currentLocation="/portfolio" />
+      </MemoryRouter>
+    );
+
+    const aboutMe = screen.getByText('About Me');
+    const github = screen.getByText('GitHub');
+    const linkedin = screen.getByText('LinkedIn');
+    const email = screen.getByText('E-Mail Me');
+
+    expect(aboutMe).toBeInTheDocument();
+    expect(github).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
+
+  it('renders portfolio link when on path /about-me', () => {
+    render(
+      <MemoryRouter>
+        <Footer currentLocation="/about-me" />
+      </MemoryRouter>
+    );
+
+    const portfolio = screen.getByText('Portfolio');
+    const github = screen.getByText('GitHub');
+    const linkedin = screen.getByText('LinkedIn');
+    const email = screen.getByText('E-Mail Me');
+
+    expect(portfolio).toBeInTheDocument();
     expect(github).toBeInTheDocument();
     expect(linkedin).toBeInTheDocument();
     expect(email).toBeInTheDocument();
