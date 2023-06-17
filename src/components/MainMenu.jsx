430e69b66ddbcb2840d9e0b18e8cde1bc514bc0e
Greg Cain <codegregcode@gmail.com>
2023-06-17 17:39:53 +0100

    adds more menu items, h1 and p

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/mainmenu.css';
+
 const MainMenu = () => {
   return (
-    <div>
+    <div className="menu-container">
+      <h2>Greg C</h2>
+      <p>
+        I love creating things, solving problems and learning, and I&apos;m
+        currently searching for a Junior Software Developer role.
+      </p>
       <Link to="/about-me">
         <h3>About Me</h3>
       </Link>
       <Link to="/portfolio">
         <h3>Portfolio</h3>
+      </Link>
+      <Link to="/photo-reel">
+        <h3>Photo Reel</h3>
+      </Link>
+      <Link to="/music">
+        <h3>Music</h3>
       </Link>
     </div>
   );
